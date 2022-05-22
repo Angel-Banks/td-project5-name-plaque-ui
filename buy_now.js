145f92f17016f6c4232545d8162e1e51f0776ef0
Angel-Banks <angelonuchuks@gmail.com>
2022-05-22 23:34:27 +0100

    updated

--- buy_now.js
+++ buy_now.js
@@ -1,30 +1,72 @@
 var message = document.querySelector("#sub-heading");
-var yourName = document.querySelector(".name");
-var output = document.querySelector(".output");
 var btn = document.querySelector(".btn");
 var guess = document.querySelector(".guess");
+  var yourName = document.querySelector(".name");
+  var output = document.querySelector(".output");
+
+  // guess.oninput = (e) =>{
+  //
+  //       var urName =  e.target.value;
+  //     yourName.textContent = urName;
+  //
+  //     var outPut = '$' + urName.replaceAll(' ', '').length*5;
+  //      output.textContent = outPut;
+  // };
 
 
-// Refactoring with Arrow Function
-  var countLetters = (data) => {
-  yourName.textContent = data
-  var price = data.replaceAll(' ', '').length * 5; // price of each letter
+  guess.addEventListener("input", function(e){
 
-  output.textContent = 'Total:$' + price
-}
-
-  guess.oninput = (e) => {
-   countLetters(e.target.value)
-}
+        var urName =  e.target.value;
+      yourName.textContent = urName;
 
 
-// function countLetters (some_data){
+  });
+
+    btn.addEventListener("click", function(e) {
+      var outPut = '$' + urName.replaceAll(' ', '').length*5;
+       output.textContent = outPut;
+
+  });
+  // function buttonAnimation (currentKey) {
+  //   var activeButton = document.querySelector ("." + currentKey) ;
+  //   activeButton.classList.add ("pressed");
+  //   setTimeout(function (){
+  //     activeButton.classList.remove ("pressed");
+  //   }, 100);
+  //
+  // }
+  //
+// // Refactoring with Arrow Function
+//   var countLetters = (data) => {
+//   yourName.textContent = data
+//   var price = data.replaceAll(' ', '').length * 5; // price of each letter
 //
-//   var price = some_data.replaceAll(' ', '').length * 5; // price of each letter
-//   yourName.textContent = some_data
+//   output.textContent = 'Total:$' + price
+// }
+//
+//   guess.oninput = (e) => {
+//    countLetters(e.target.value)
+// }
+
+//
+// function countLetters (data){
+//
+//   var price = data.replaceAll(' ', '').length * 5; // price of each letter
+//   yourName.textContent = data
 //   output.textContent = 'Total:$'+ price
 // }
 //
 // guess.addEventListener("input", function(e){
 //     countLetters(e.target.value)
+// });
+
+
+//
+// guess.addEventListener("input", function(e){
+//
+//       var urName =  e.target.value;
+//     yourName.textContent = urName;
+//
+//     var outPut = '$' + urName.replaceAll(' ', '').length*5;
+//      output.textContent = outPut;
 // });
