13c8c4b87ef4c16099eff22a2f79679acccc3aa2
Angel-Banks <angelonuchuks@gmail.com>
2022-05-28 16:53:11 +0100

    updated

--- buy_now.js
+++ buy_now.js
@@ -15,20 +15,31 @@ guess.oninput = () => {
 
 // The buyNow function
 
-  var buyNow = buyButton.onclick= () => {
-    // buyButton.addEventListener('click', function buyNow() {
+var buyNow = buyButton.onclick = () => {
+  // buyButton.addEventListener('click', function buyNow() {
+  buyButton.classList.add("pressed")
+  setTimeout(function() {
+    buyButton.classList.remove("pressed");
+  }, 100);
+
   var outPut = guess.value;
 
-  if (guess.value === '') {
+  buyButton.classList.add("pressed")
+  setTimeout(function() {
+    buyButton.classList.remove("pressed");
+  }, 100);
+
+
+  if (outPut === '') {
     output.textContent = 'Total:$0';
 
-  } else if (guess.value.length < 16) {
+  } else if (outPut.length < 16) {
     output.textContent = 'Total:$' + outPut.replaceAll(' ', '').length * 5;
 
     alertMessage.textContent = "🚩Congratulations, you entered a unique letter!"
     alertMessage.style.color = "#CE49BF";
 
-  } else if (guess.value.length > 15) {
+  } else if (outPut.length > 15) {
     output.textContent = 'Total:$0';
 
     alertMessage.textContent = "Oops, you have exceeded the number of available letters!"
@@ -40,8 +51,13 @@ guess.oninput = () => {
 
 // The Reset function
 
-var resetPlaque = resetButton.onclick= () => {
-// resetButton.addEventListener('click', function resetPlaque() {
+var resetPlaque = resetButton.onclick = () => {
+  // resetButton.addEventListener('click', function resetPlaque() {
+  resetButton.classList.add("pressed")
+  setTimeout(function() {
+    resetButton.classList.remove("pressed");
+  }, 100);
+
   guess.value = '';
   nameSpace.textContent = 'Your Name';
   output.textContent = 'Total:$0';
@@ -49,15 +65,3 @@ var resetPlaque = resetButton.onclick= () => {
   alertMessage.style.color = "#68A7AD";
   // window.location.reload();
 };
-
-
-
-
-// function () {
-//   var activeButton = document.querySelector (".btn") ;
-//   activeButton.classList.add ("pressed")
-//   setTimeout(function (){
-//     activeButton.classList.remove ("pressed");
-//   }, 100);
-//
-// }
